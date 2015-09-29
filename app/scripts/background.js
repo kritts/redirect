f03912a785bd6063b768e5fea248cea9c9c537c8
Krittika DSilva <krittika.dsilva@gmail.com>
2015-09-28 23:38:43 -0700

    Adding images of icon and working on logic for Redirect.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,7 +1,26 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
-});
+// This event is fired each time the user updates the text in the omnibox,
+// as long as the extension's keyword mode is still active.
+chrome.omnibox.onInputChanged.addListener(
+  function(text, suggest) {
+    var url = localStorage[text];
+    if(url == undefined) {
+        chrome.omnibox.setDefaultSuggestion({"description" : "No redirect found."});
+    } else {
+        chrome.omnibox.setDefaultSuggestion({"description" : "Redirecting you to: " + localStorage[text]});
+    }
+  }
+);
 
-console.log('\'Allo \'Allo! Event Page');
+// This event is fired with the user accepts the input in the omnibox.
+chrome.omnibox.onInputEntered.addListener(
+  function(text) {
+      var urlval = localStorage[text];
+      console.log(urlval);
+    if(urlval != undefined) {
+        chrome.tabs.update({url: urlval});
+    }
+  }
+);
+
